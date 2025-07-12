1a5c89e7d9265f02511170650c322fe690688b3f
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-13 09:38:54 +1000

    Update main.dart

--- android_app_flutter/webview/main.dart
+++ android_app_flutter/webview/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: WebView(
+            initialUrl: 'http://your-streamlit-url:8501',
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
