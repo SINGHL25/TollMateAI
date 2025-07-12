9bd38c626c09627cdad19517b21287826b6fca99
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-13 09:30:31 +1000

    Update fraud_model.py

--- ml_models/fraud_model.py
+++ ml_models/fraud_model.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+def detect_fraud(df_transaction):
+    fraud_cases = df_transaction[
+        (df_transaction['Matched'] == 'No') |
+        (df_transaction['Transaction Completed'] == 'No') |
+        (df_transaction['Serial Number'] == 0)
+    ]
+    fraud_cases['fraud_type'] = fraud_cases.apply(lambda row: "Unmatched" if row['Matched'] == 'No' else (
+        "Incomplete" if row['Transaction Completed'] == 'No' else "Invalid Serial"), axis=1)
+    return fraud_cases
 
