c003db885e4c50ffbbb3452129749d2406b2c083
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-13 09:08:45 +1000

    Update mongo_config.py

--- mongodb_integration/mongo_config.py
+++ mongodb_integration/mongo_config.py
@@ -1,1 +1,9 @@
+from pymongo import MongoClient
+
+def get_mongo_client():
+    return MongoClient("mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority")
+
+def get_database():
+    client = get_mongo_client()
+    return client["tollmateai"]
 
