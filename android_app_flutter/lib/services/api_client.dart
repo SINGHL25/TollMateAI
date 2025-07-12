2b1963b5c25736e446336d28a46838d2dd4990ad
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-13 09:54:45 +1000

    Update api_client.dart

--- android_app_flutter/lib/services/api_client.dart
+++ android_app_flutter/lib/services/api_client.dart
@@ -1,1 +1,24 @@
+```dart
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
+class ApiClient {
+  final String baseUrl;
+
+  ApiClient({required this.baseUrl});
+
+  Future<Map<String, dynamic>> uploadFile(File file, String endpoint) async {
+    final uri = Uri.parse('$baseUrl/model/$endpoint');
+    final request = http.MultipartRequest('POST', uri);
+    request.files.add(await http.MultipartFile.fromPath('file', file.path));
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      final body = await response.stream.bytesToString();
+      return jsonDecode(body);
+    } else {
+      throw Exception('Failed to upload file');
+    }
+  }
+}
