880fdf3ba626f19b6a4ad40203cc9b5c2fed58e3
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-13 10:35:44 +1000

    Update traffic_bar_chart.dart

--- android_app_flutter/lib/charts/traffic_bar_chart.dart
+++ android_app_flutter/lib/charts/traffic_bar_chart.dart
@@ -1,1 +1,32 @@
+```dart
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
+class TrafficBarChart extends StatelessWidget {
+  final Map<String, double> data;
+
+  TrafficBarChart({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        alignment: BarChartAlignment.spaceAround,
+        maxY: (data.values.reduce((a, b) => a > b ? a : b)) + 10,
+        barTouchData: BarTouchData(enabled: false),
+        titlesData: FlTitlesData(
+          bottomTitles: SideTitles(showTitles: true, getTitles: (double value) {
+            return data.keys.elementAt(value.toInt());
+          }),
+          leftTitles: SideTitles(showTitles: true),
+        ),
+        borderData: FlBorderData(show: false),
+        barGroups: data.entries.mapIndexed((i, entry) {
+          return BarChartGroupData(x: i, barRods: [
+            BarChartRodData(y: entry.value, colors: [Colors.blue])
+          ]);
+        }).toList(),
+      ),
+    );
+  }
+}
